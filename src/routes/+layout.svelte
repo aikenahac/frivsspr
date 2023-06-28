dbd02c384fb9a8eeaf4305208721fa7cc0262266
Aiken Tine Ahac <ahac.aiken@gmail.com>
2023-06-28 17:38:38 +0200

    Migrate to svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import "../app.postcss";
+</script>
+
+<svelte:head>
+  <title>FRI VSŠ Predmetnik</title>
+</svelte:head>
+
+<main>
+  <div class="navbar bg-base-100">
+    <div class="flex-1">
+      <p class="btn btn-ghost normal-case text-xl">FRIVSŠPr</p>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><a href="/">Seznam</a></li>
+        <li>
+          <a
+            href="https://www.fri.uni-lj.si/upload/Slike/predmetniki/2021_predmetnik_vss_ri.pdf"
+            target="_blank">Vizualni pregled</a
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+  <slot />
+</main>
