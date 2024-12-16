4e2f72815b5e6d06c87dfc0e54ef9f139206dd17
Aiken Tine Ahac <ahac.aiken@gmail.com>
2024-12-16 12:34:30 +0100

    Show rating inline on fp, rate subject on expanded fp

--- src/lib/components/SubjectTile.svelte
+++ src/lib/components/SubjectTile.svelte
@@ -1,6 +1,31 @@
 <script lang="ts">
   import type { Subject } from '$lib/types';
   import { SubjectType } from '$lib/types';
+  import { onMount } from 'svelte';
+
+  let rating: string | undefined = undefined;
+  let hasVoted = true;
+  let vote = 0;
+  let voteCount: number;
+
+  onMount(() => {
+    const hv: boolean = JSON.parse(
+      localStorage.getItem(`hasVoted-${subject.id}`) || 'false',
+    );
+    const vt: number = parseInt(
+      localStorage.getItem(`vote-${subject.id}`) || '0',
+    );
+
+    hasVoted = hv;
+    vote = vt;
+    rating = (
+      subject.ratings && subject.voteCount
+        ? subject.ratings.reduce((a, c) => a + c, 0) /
+          subject.voteCount
+        : 0
+    ).toFixed(2);
+    voteCount = subject.ratings?.length ?? 0;
+  });
 
   function higlightSubject(id: number | null) {
     if (!id) return;
@@ -43,10 +68,13 @@
   <div id={subject.id.toString()} class="collapse collapse-arrow bg-base-200">
     <input type="checkbox" />
     <div
-      class="collapse-title text-xl font-medium {getSubjectColor(subject.type)}"
+      class="collapse-title text-xl font-medium {getSubjectColor(subject.type)} flex flex-row items-center justify-between"
     >
       <div class="flex flex-row items-center justify-items-center">
         {subject.name}
+        {#if rating && voteCount > 0}
+          <span class="text-sm ml-3">[{rating}/5 ({voteCount})]</span>
+        {/if}
         {#if subject.notTaught}
           <div class="badge badge-error ml-4">Se ne izvaja</div>
         {/if}
@@ -101,6 +129,57 @@
             </li>
           {/each}
         </ul>
+      {/if}
+      {#if rating}
+        <div class="mt-2">
+        <p>Ocena: {rating} ({voteCount})</p>
+        <div class="rating rating-lg">
+          <input
+            disabled={hasVoted}
+            type="radio"
+            name="rating-1"
+            class="mask mask-star"
+            checked={vote === 1}
+            on:click={() => (vote = 1)}
+          />
+          <input
+            disabled={hasVoted}
+            type="radio"
+            name="rating-1"
+            class="mask mask-star"
+            checked={vote === 2}
+            on:click={() => (vote = 2)}
+          />
+          <input
+            disabled={hasVoted}
+            type="radio"
+            name="rating-1"
+            class="mask mask-star"
+            checked={vote === 3}
+            on:click={() => (vote = 3)}
+          />
+          <input
+            disabled={hasVoted}
+            type="radio"
+            name="rating-1"
+            class="mask mask-star"
+            checked={vote === 4}
+            on:click={() => (vote = 4)}
+          />
+          <input
+            disabled={hasVoted}
+            type="radio"
+            name="rating-1"
+            class="mask mask-star"
+            checked={vote === 5}
+            on:click={() => (vote = 5)}
+          />
+          <button disabled={hasVoted} class="btn ml-2" on:click={() => submitVote()}
+            >Glasuj</button
+          >
+          <div class="divider" />
+        </div>
+        </div>
       {/if}
     </div>
   </div>
